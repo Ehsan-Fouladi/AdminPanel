a6ab5c465e391a1ce4a127f949efd89f89e788d1
Ehsan-Fouladi <fooehan3@gmail.con>
2025-02-18 02:26:39 +0330

    create table and table user

---
+++ pages/users/index.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <div class="card">
+      <Button class="mb-2">افزودن کاربر جدید</Button>
+      <DataTable :value="customers" tableStyle="min-width: 50rem">
+        <Column field="name" header="نام"></Column>
+        <Column field="family" header="نام خانوادگی"></Column>
+        <Column field="email" header="ایمیل"></Column>
+        <Column field="phoneNumber" header="شماره تلفن"></Column>
+        <template #footer>
+          <div class="w-full flex justify-center items-center">
+            <BasePagination/>
+          </div>
+        </template>
+      </DataTable>
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+definePageMeta({
+  title: "مدیریت کاربران"
+})
+
+const customers = ref([
+  {
+    name: "احسان",
+    family: "فولادی",
+    email: "example@gmail.com",
+    phoneNumber: "09123456789"
+  }
+])
+</script>
