ac46f2c4a0f80189d686c8f99d4adacc4f24512a
Ehsan Fouladi Developer <112898322+Ehsan-Fouladi@users.noreply.github.com>
2025-11-07 02:01:35 +0330

    set grid custom

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,9 @@ export default {
   plugins: [require("tailwindcss-primeui")],
   theme: {
     extend: {
+      gridTemplateColums:{
+        "feature-produc":"repeat(auto-fit, minmax(200px, 1fr))"
+      },
       screens: {
         xl: { max: "1536px" },
         lg: { max: "1280px" },
